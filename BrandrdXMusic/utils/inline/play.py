6fb0dd4be293e40ca6bee4e5a4d091e103a0b0a4
Devilcode53 <japusahu7@gmail.com>
2025-03-12 09:42:37 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Want_To_Know_Me"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Devil_codes"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/learningbots79"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+eBHWW1mTPHQ5ZmE9"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Want_To_Know_Me"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Devil_codes"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/learningbots79"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+eBHWW1mTPHQ5ZmE9"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -245,10 +245,10 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Want_To_Know_Me"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Devil_codes"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/learningbots79"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/+eBHWW1mTPHQ5ZmE9"
             ),
         ],
 
